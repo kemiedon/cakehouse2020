5e722feb493eb2169132d757cd43b8c7935e29cf
kemiedon <kemiedon@gmail.com>
2020-05-20 09:09:27 +0800

    1

--- admin/functions/login_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['user']) && $_SESSION['user'] == null ){
-    header('Location: ../login.php?Msg=LoginFirst');
-}
-
-?>
--- web/cart/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$i = $_GET['cart_id'];
-unset($_SESSION['Cart'][$i]);
-$_SESSION['Cart'] = array_values($_SESSION['Cart']);
-header('Location: ../basket.php?Del=true');
-?>
